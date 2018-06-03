e4a6b281be84ef8f5bc6315651d303937be96643
Juan Campos <juanignacioco@gmail.com>
2018-06-03 02:17:26 +0000

    Main java program added

---
+++ src/OpenHugs.java
@@ -0,0 +1,10 @@
+
+
+public class OpenHugs {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(arg[0]);
+        Parser parser = new Parser(scanner);
+        parser.Parse();
+        System.out.println(parser.errors.count + " errors detected");
+    }   
+}   
