1a778f16b090668d09407510cf94b8f361422336
Geovanny Cordero <28696479+geovannycordero@users.noreply.github.com>
2018-07-11 22:09:15 +0000

    tag generation

--- src/main/java/code_generation/TagManager.java
+++ src/main/java/code_generation/TagManager.java
@@ -1,5 +1,23 @@
-package main.java.code_generation;
+//pakage main.java.code_generation;
 
 public class TagManager {
-
+    private int tnum;
+    private final String tname = "tag";
+    
+    public TagManager(){
+        tnum = 0;
+    }
+    
+    public String getTag(){
+        return tname + tnum++;
+    }
+    
+    public static void main(String [] args){
+        TagManager t = new TagManager();
+        System.out.println(t.getTag());
+        System.out.println(t.getTag());
+        System.out.println(t.getTag());
+        System.out.println(t.getTag());
+        System.out.println(t.getTag());
+    }
 }
